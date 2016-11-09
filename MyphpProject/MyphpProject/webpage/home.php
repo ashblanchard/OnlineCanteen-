bba188b3cc9a7e24d8f55a1e3a6662c1190f4406
Patrick McGee <p0998@outlook.com>
2016-11-09 17:09:59 -0400

    cleaning up files
    contains old fwebpage files, also my new clean files within the webpage
    folder.

--- MyphpProject/MyphpProject/webpage/home.php
+++ MyphpProject/MyphpProject/webpage/home.php
@@ -3,6 +3,23 @@
 Author: Patrick McGee
 Date: 9/11/2016
 -->
+<!--SHELL
+<html>
+    <head>
+        <title>Login Page</title>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <link href ="styles.css" type ="text/css" rel ="stylesheet"/>
+        <script src ="scripts.js"></script>
+    </head>
+    <body>
+        <div class = "container">
+
+
+        </div>
+    </body>
+</html>
+-->
 <html>
     <head>
         <title>Login Page</title>
@@ -23,14 +40,18 @@ Date: 9/11/2016
                 <h1 id = "homeBannerText">Camp Seggie's Online Canteen</h1>
             </div>
             <hr>
+            <br><br><br><br>
             <!--Fix this to be a bigger search bar-->
             <div id ="homeSearchDiv">
-                Search Directory:<br>
+                <h3>Search Directory:</h3><br>
                 <form id ="homeSearch">
-                    <input id ="homeSearchBar" type ="search"><button type ="button">Search</button>
+                    <input id ="homeSearchBar" type ="search"style="height: 30px; width: 500px;">
+                    <button type ="button" style="height: 30px; width: 100px;">Search</button>
                 </form>
             </div>
-
+            <br><br><br><br>
+            <br><br><br><br>
+            <a href ="login.html">Back to login</a>
         </div>
     </body>
 </html>
---
+++ MyphpProject/MyphpProject/webpage/settingsPage.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<!--
+Author: Patrick McGee
+Date: 9/11/2016
+-->
+
+<html>
+    <head>
+        <title>Login Page</title>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <link href ="styles.css" type ="text/css" rel ="stylesheet"/>
+        <script src ="scripts.js"></script>
+    </head>
+    <body>
+        <div class = "container">
+            <h1>Settings</h1>
+            
+            
+            
+
+        </div>
+    </body>
+</html>
