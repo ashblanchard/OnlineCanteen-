cab2a3f68c6d66d79663c2f7b731c4b582b7c709
Patrick McGee <p0998@outlook.com>
2016-11-10 11:36:25 -0400

    Login Connected to database
    May have to create another database.

---
+++ login09/demo/config.php
@@ -0,0 +1,16 @@
+<?php
+
+
+$db_host = 'localhost';
+$db_port = 3306;
+$db_user = 'camp';
+$db_pass = 'camp';
+$db_name = 'campseggie';
+
+
+/* 
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
